9fee1a569128fd8513f2440f0cb0723f12a2fdcb
Untamedy <Untamedy@bigmir.net>
2019-10-05 23:26:13 +0300

    no message

--- src/main/java/com/springApp/controller/PhotoController.java
+++ src/main/java/com/springApp/controller/PhotoController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @author Lenovo
  */
 @Controller
-@RequestMapping("/photo")
+@RequestMapping("/")
 public class PhotoController {
     
     @Autowired
